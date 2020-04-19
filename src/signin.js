5faabdcf0d2a8d053f9211d053473173e4949d6a
Gaby Alvarez <gabyalvarzb@gamil.com>
2020-04-19 08:18:06 -0500

    add function cunsult user

--- src/signin.js
+++ src/signin.js
@@ -121,6 +121,42 @@ function sentEmailConfirmation() {
   });
 }
 
+/**************************** */
+
+function consultarUsuarios(){
+  dataBase.collection("users").get()
+    .then(function(data) {
+      data.forEach(function(user) {
+          console.log(user.id);
+          let detalleUser = user.data();
+          console.log(detalleUser.name);
+          console.log(detalleUser.lastName);
+          console.log(detalleUser.email);
+          console.log("****************")
+      });
+    }).catch(function()  {
+      console.error("Error al consultar la BDD")
+  });
+}
+
+function consultarUsuario(campo, value){
+  dataBase.collection("users").where(campo, "==", value)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error al consultar por correo");
+    });
+
+}
+
+//consultarUsuarios();
+consultarUsuario("email","gabyalvarzb@gmail.com");
+//consultarUsuario("name","gabs");
+
 /*
 
 function outlogin() {
